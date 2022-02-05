564d77e0e97e53759a104ef9ea83c0e928ce605d
mdmubin <mdmubin@linux.com>
2022-02-05 19:30:56 +0600

    forgot to add the file handler doc :P

--- include/utils/FileIO.hpp
+++ include/utils/FileIO.hpp
@@ -4,5 +4,12 @@
 
 namespace myutils
 {
+    /**
+     * @brief Read all the file contents from a file and return the contents
+     * within a std::string
+     *
+     * @param filepath path to the file
+     * @return std::string containing the contents of the file
+     */
     std::string readFileContents(std::string &filepath);
 }
